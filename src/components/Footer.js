d908c02f6db6cb29b8b1f63bfce60d16206753f1
Daniel Chorągwicki <choragwickid@gmail.com>
2018-09-06 16:19:50 +0200

    production config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
+
 export default props => (
     <div>
         <Link to='/'>All</Link>
         <Link to='/active'>Active</Link>
         <Link to='/completed'>Completed</Link>
-    </div>
+    </div> 
 )
